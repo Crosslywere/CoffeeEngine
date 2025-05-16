6ed94f04e2d0d4a86bc8832bd544e7576810bfdb
Crosslywere <ogboruj@outlook.com>
2025-05-16 22:43:50 +0100

    Updated file reading function to be simpler/shorter
    Signed-off-by: Crosslywere <ogboruj@outlook.com>

--- src/main/java/com/crossly/util/FileUtil.java
+++ src/main/java/com/crossly/util/FileUtil.java
@@ -1,24 +1,33 @@
 package com.crossly.util;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileUtil {
 
     public static String getFileString(String path) {
-        try (var stream = FileUtil.class.getClassLoader().getResourceAsStream(path)) {
-            if (stream == null) {
-                throw new RuntimeException("File '" + path + "' not found!");
-            }
-            var reader = new BufferedReader(new InputStreamReader(stream));
-            StringBuilder source = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                source.append(line).append('\n');
-            }
-            return source.toString();
-        } catch (Exception e) {
+        return new String(getFileBytes(path));
+    }
+
+    public static byte[] getFileBytes(String path) {
+        try {
+            return Files.readAllBytes(Paths.get(getAbsoluteFilepath(path)));
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static String getAbsoluteFilepath(String path) {
+        var url = FileUtil.class.getClassLoader().getResource(path);
+        if (url != null)
+            return URLDecoder.decode(url.getFile().substring(System.getProperty("os.name").contains("Windows") ? 1 : 0), Charset.defaultCharset());
+        File file = new File(path);
+        if (!file.isFile())
+            throw new RuntimeException("File '" + path + "' does not exist!");
+        return file.getAbsolutePath();
     }
 }
