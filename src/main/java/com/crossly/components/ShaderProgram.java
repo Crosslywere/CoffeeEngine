e6db0af224b3e1e956497e2a2954f769fa03e7d9
Crosslywere <ogboruj@outlook.com>
2025-05-16 22:47:09 +0100

    Implemented uniform setting functions and added more shader type support to builder. - Compute shaders - Geometry shaders - Tesselation evaluation shaders - Tesselation control shaders
    Signed-off-by: Crosslywere <ogboruj@outlook.com>

--- src/main/java/com/crossly/components/ShaderProgram.java
+++ src/main/java/com/crossly/components/ShaderProgram.java
@@ -2,11 +2,11 @@ package com.crossly.components;
 
 import com.crossly.interfaces.Component;
 import com.crossly.util.FileUtil;
+import org.joml.Matrix4f;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.lwjgl.opengl.GL46.*;
 
@@ -14,6 +14,9 @@ public class ShaderProgram implements Component {
 
     private final int program;
     private final Map<String, Integer> uniformLocationCache = new HashMap<>();
+    private int projectionUniformLocation = -1;
+    private int viewUniformLocation = -1;
+    private int modelUniformLocation = -1;
 
     public ShaderProgram(String vertexSource, String fragmentSource) {
         this.program = builder()
@@ -24,6 +27,102 @@ public class ShaderProgram implements Component {
 
     private ShaderProgram(int program) {
         this.program = program;
+    }
+
+    private int getUniformLocation(String name) {
+        if (!uniformLocationCache.containsKey(name)) {
+            int loc = glGetUniformLocation(program, name);
+            uniformLocationCache.put(name, loc);
+            if (loc < 0) System.out.println("uniform '" + name + "' is unused/unavailable in shader.");
+            return loc;
+        }
+        return uniformLocationCache.get(name);
+    }
+
+    public void setInt(String name, int value) {
+        glUniform1i(getUniformLocation(name), value);
+    }
+
+    public void setInts(String name, int... values) {
+        glUniform1iv(getUniformLocation(name), values);
+    }
+
+    public void setFloat(String name, float value) {
+        glUniform1f(getUniformLocation(name), value);
+    }
+
+    public void setFloats(String name, float... values) {
+        glUniform1fv(getUniformLocation(name), values);
+    }
+
+    public void setFloat2(String name, float x, float y) {
+        glUniform2f(getUniformLocation(name), x, y);
+    }
+
+    public void setFloat2(String name, Vector2f value) {
+        setFloat2(name, value.x(), value.y());
+    }
+
+    public void setFloat2s(String name, float... values) {
+        assert values.length % 2 == 0;
+        glUniform2fv(getUniformLocation(name), values);
+    }
+
+    public void setFloat3(String name, float x, float y, float z) {
+        glUniform3f(getUniformLocation(name), x, y, z);
+    }
+
+    public void setFloat3(String name, Vector3f value) {
+        setFloat3(name, value.x(), value.y(), value.z());
+    }
+
+    public void setFloat3s(String name, float... values) {
+        assert values.length % 3 == 0;
+        glUniform3fv(getUniformLocation(name), values);
+    }
+
+    public void setMat4(String name, float... values) {
+        assert values.length == 16;
+        glUniformMatrix4fv(getUniformLocation(name), false, values);
+    }
+
+    public void setMat4(String name, Matrix4f matrix) {
+        float[] values = new float[16];
+        matrix.get(values);
+        glUniformMatrix4fv(getUniformLocation(name), false, values);
+    }
+
+    public void setProjectionUniform(String name) {
+        projectionUniformLocation = getUniformLocation(name);
+    }
+
+    public void setProjectionMatrix(Matrix4f matrix) {
+        if (projectionUniformLocation == -1) return;
+        float[] values = new float[16];
+        matrix.get(values);
+        glUniformMatrix4fv(projectionUniformLocation, false, values);
+    }
+
+    public void setViewUniform(String name) {
+        viewUniformLocation = getUniformLocation(name);
+    }
+
+    public void setViewMatrix(Matrix4f matrix) {
+        if (viewUniformLocation == -1) return;
+        float[] values = new float[16];
+        matrix.get(values);
+        glUniformMatrix4fv(viewUniformLocation, false, values);
+    }
+
+    public void setModelUniform(String name) {
+        modelUniformLocation = getUniformLocation(name);
+    }
+
+    public void setModelMatrix(Matrix4f matrix) {
+        if (modelUniformLocation == -1) return;
+        float[] values = new float[16];
+        matrix.get(values);
+        glUniformMatrix4fv(modelUniformLocation, false, values);
     }
 
     @Override
@@ -70,6 +169,46 @@ public class ShaderProgram implements Component {
             return attachFragmentShader(src);
         }
 
+        public Builder attachComputeShader(String src) {
+            shaderParts.add(compileShaderFromMemory(src, GL_COMPUTE_SHADER));
+            return this;
+        }
+
+        public Builder attachComputeShaderFile(String srcPath) {
+            String src = FileUtil.getFileString(srcPath);
+            return attachComputeShader(src);
+        }
+
+        public Builder attachGeometryShader(String src) {
+            shaderParts.add(compileShaderFromMemory(src, GL_GEOMETRY_SHADER));
+            return this;
+        }
+
+        public Builder attachGeometryShaderFile(String srcPath) {
+            String src = FileUtil.getFileString(srcPath);
+            return attachGeometryShader(src);
+        }
+
+        public Builder attachTesselationEvaluationShader(String src) {
+            shaderParts.add(compileShaderFromMemory(src, GL_TESS_EVALUATION_SHADER));
+            return this;
+        }
+
+        public Builder attachTesselationEvaluationShaderFile(String srcPath) {
+            String src = FileUtil.getFileString(srcPath);
+            return attachTesselationEvaluationShader(src);
+        }
+
+        public Builder attachTesselationControlShader(String src) {
+            shaderParts.add(compileShaderFromMemory(src, GL_TESS_CONTROL_SHADER));
+            return this;
+        }
+
+        public Builder attachTesselationControlShaderFile(String srcPath) {
+            String src = FileUtil.getFileString(srcPath);
+            return attachTesselationControlShader(src);
+        }
+
         public ShaderProgram create() {
             for (int part : shaderParts) {
                 glAttachShader(program, part);
@@ -79,7 +218,7 @@ public class ShaderProgram implements Component {
             return new ShaderProgram(program);
         }
 
-        public ShaderProgram createCopy() {
+        public ShaderProgram createClone() {
             int program = glCreateProgram();
             for (int part : shaderParts) {
                 glAttachShader(program, part);
