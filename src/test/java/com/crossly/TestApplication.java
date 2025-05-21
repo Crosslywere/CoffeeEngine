9f2f79dea270e3016d7d5e32531085234db92075
Crosslywere <ogboruj@outlook.com>
2025-05-21 17:07:31 +0100

    Implemented an instance of Camera3D
    Signed-off-by: Crosslywere <ogboruj@outlook.com>

--- src/test/java/com/crossly/TestApplication.java
+++ src/test/java/com/crossly/TestApplication.java
@@ -1,50 +1,32 @@
 package com.crossly;
 
-import com.crossly.components.FrameBuffer;
+import com.crossly.components.Framebuffer;
 import com.crossly.components.Mesh;
 import com.crossly.components.ShaderProgram;
+import com.crossly.entity.Camera3D;
 import com.crossly.entity.Entity;
 import com.crossly.input.Input;
 import com.crossly.interfaces.Application;
-import org.joml.Matrix4f;
 
 import static org.lwjgl.glfw.GLFW.glfwGetTime;
 
 public class TestApplication extends Application {
 
     private final Entity entity;
+    private final Camera3D camera;
 
     public TestApplication() {
         super(800, 600, "Test Application!");
-        ShaderProgram testTreeShader = new ShaderProgram(
-                """
-                        #version 460 core
-                        layout (location = 0) in vec3 aPos;
-                        layout (location = 3) in vec3 aTexCoord;
-                        uniform mat4 proj;
-                        uniform mat4 model;
-                        out vec3 texCoord;
-                        void main() {
-                            texCoord = aTexCoord;
-                            gl_Position = proj * model * vec4(aPos, 1.0);
-                        }""",
-                """
-                        #version 460 core
-                        layout (location = 0) out vec4 oFragColor;
-                        in vec3 texCoord;
-                        void main() {
-                            oFragColor = vec4(abs(texCoord), 1.0);
-                        }"""
-        );
-        Matrix4f projection = new Matrix4f()
-                .perspective((float) Math.toRadians(60), 4f / 3f, 0.1f, 100)
-                .translate(0, -1, -4).rotateX((float) Math.toRadians(30));
+        ShaderProgram testTreeShader = ShaderProgram.builder()
+                .attachVertexShaderFile("test/shader/shader.vert")
+                .attachFragmentShaderFile("test/shader/shader.frag")
+                .build();
         testTreeShader.use();
         testTreeShader.setProjectionUniform("proj");
         testTreeShader.setModelUniform("model");
-        testTreeShader.setProjectionMatrix(projection);
         Mesh.Buffer testMeshBuffer = Mesh.Buffer.create("test/mesh/ChessPiece.obj");
         entity = new Entity(testTreeShader, testMeshBuffer);
+        camera = new Camera3D((float) getWindowWidth() / getWindowHeight(), 60);
     }
 
     public void onUpdate(Input input) {
@@ -54,13 +36,21 @@ public class TestApplication extends Application {
     }
 
     public void onRender() {
-        FrameBuffer.clear();
+        Framebuffer.clear();
+        entity.getShaderComponent().ifPresent(
+                shaderProgram -> shaderProgram.setProjectionMatrix(camera.getProjectionView())
+        );
         entity.render();
     }
 
     @Override
     public void onExit() {
         entity.cleanup();
+    }
+
+    @Override
+    public void onResize() {
+        camera.setAspectRatio((float) getWindowWidth() / getWindowHeight());
     }
 
     public static void main(String... args) {
