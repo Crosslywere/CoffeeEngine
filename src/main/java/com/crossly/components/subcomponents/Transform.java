d3dd189da1aef0286455d01cecef0ca244548a39
Crosslywere <ogboruj@outlook.com>
2025-05-28 12:55:23 +0100

    Implemented transform orientation
    Signed-off-by: Crosslywere <ogboruj@outlook.com>

--- src/main/java/com/crossly/components/subcomponents/Transform.java
+++ src/main/java/com/crossly/components/subcomponents/Transform.java
@@ -13,26 +13,39 @@ public class Transform implements SubComponent {
     private Vector3f position = new Vector3f();
     private Vector3f rotation = new Vector3f();
     private Vector3f scale = new Vector3f(1);
+    private final Orientation orientation;
 
     private Transform parent = null;
 
     public Transform() {
+        orientation = new Orientation(rotation);
+    }
+
+    public Transform(Vector3f position, Vector3f rotation, Vector3f scale) {
+        this.position = position;
+        this.rotation = rotation;
+        this.scale = scale;
+        orientation = new Orientation(rotation);
+    }
+
+    public static Vector3f getWorldUp() {
+        return new Vector3f(Orientation.WORLD_UP);
     }
 
     private static Vector3f rotate(Vector3f vector, Vector3f rotation) {
         return vector.rotate(new Quaternionf().rotateXYZ(rotation.x(), rotation.y(), rotation.z()));
     }
 
-    public Vector3f getForward() {
-        return rotate(new Vector3f(0, 0, 1), rotation);
+    public Vector3f getFront() {
+        return orientation.getFront();
     }
 
     public Vector3f getUp() {
-        return rotate(new Vector3f(0, 1, 0), rotation);
+        return orientation.getUp();
     }
 
     public Vector3f getRight() {
-        return rotate(new Vector3f(1, 0, 0), rotation);
+        return orientation.getRight();
     }
 
     public Matrix4f getTransformMatrix() {
@@ -54,7 +67,7 @@ public class Transform implements SubComponent {
     }
 
     public Vector3f getPosition() {
-        return position;
+        return new Vector3f(position);
     }
 
     public void setPosition(Vector3f position) {
@@ -78,31 +91,48 @@ public class Transform implements SubComponent {
     }
 
     public Vector3f getRotation() {
-        return rotation;
+        return new Vector3f(rotation);
     }
 
     public void setRotation(Vector3f rotation) {
         this.rotation = rotation;
+        orientation.update(rotation);
     }
 
     public void incrementRotation(Vector3f rotation) {
         this.rotation.add(rotation);
+        orientation.update(rotation);
     }
 
-    public void setRotationX(float x) {
+    public float getPitch() {
+        return rotation.x();
+    }
+
+    public void setPitch(float x) {
         rotation.x = x;
+        orientation.update(rotation);
     }
 
-    public void setRotationY(float y) {
+    public float getYaw() {
+        return rotation.y();
+    }
+
+    public void setYaw(float y) {
         rotation.y = y;
+        orientation.update(rotation);
     }
 
-    public void setRotationZ(float z) {
+    public float getRoll() {
+        return rotation.z();
+    }
+
+    public void setRoll(float z) {
         rotation.z = z;
+        orientation.update(rotation);
     }
 
     public Vector3f getScale() {
-        return scale;
+        return new Vector3f(scale);
     }
 
     public void setScale(Vector3f scale) {
@@ -132,10 +162,45 @@ public class Transform implements SubComponent {
         scale = new Vector3f(1);
     }
 
-    @Override
-    public String toString() {
-        return "TRANSLATION {" + position.x() + ", " + position.y() + ", " + position.z() + "}\n" +
-                "ROTATION {" + (float) Math.toDegrees(rotation.x()) + ", " + (float) Math.toDegrees(rotation.y()) + ", " + (float) Math.toDegrees(rotation.z()) + "}\n" +
-                "SCALE {" + scale.x() + ", " + scale.y() + ", " + scale.z() + "}";
+    public static class Orientation {
+        private final Vector3f front = new Vector3f();
+        private final Vector3f right = new Vector3f();
+        private final Vector3f up = new Vector3f();
+
+        private static final Vector3f WORLD_UP = new Vector3f(0, 1, 0);
+        private static final Vector3f WORLD_RIGHT = new Vector3f(1, 0, 0);
+        private static final Vector3f WORLD_FRONT = new Vector3f(0, 0, 1);
+
+        private Orientation(Vector3f rotation) {
+            update(rotation);
+        }
+
+        private Vector3f getFront() {
+            return new Vector3f(front);
+        }
+
+        private Vector3f getRight() {
+            return new Vector3f(right);
+        }
+
+        private Vector3f getUp() {
+            return new Vector3f(up);
+        }
+
+        private void update(Vector3f rotation) {
+            update(rotation.x(), rotation.y(), rotation.z());
+        }
+
+        public void update(float pitch, float yaw, float roll) {
+            Vector3f front = new Vector3f(0, 0, 1);
+            front.rotateX(pitch);
+            front.rotateY(yaw);
+            front.normalize(this.front);
+            front.cross(WORLD_UP, right);
+            right.rotateZ(roll);
+            right.normalize();
+            right.cross(front, up);
+            up.normalize();
+        }
     }
 }
